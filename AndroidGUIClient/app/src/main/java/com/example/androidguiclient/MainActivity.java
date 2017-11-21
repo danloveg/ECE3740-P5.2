847e99223b209010e6916495ce3010b3fb5c8549
Daniel Lovegrove <d.lovegrove11@gmail.com>
2017-11-21 12:33:22 -0600

    MainActivity implements userinterface interface

--- AndroidGUIClient/app/src/main/java/com/example/androidguiclient/MainActivity.java
+++ AndroidGUIClient/app/src/main/java/com/example/androidguiclient/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.androidguiclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements userinterface.Userinterface {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Update the message text area with a new message.
+     * @param message The message to display.
+     */
+    public void update(String message) {
+        TextView textView = findViewById(R.id.messageTextArea);
+        textView.setText(message);
     }
 }
