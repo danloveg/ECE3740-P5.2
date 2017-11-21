7a64d82e447923f932bf4e92692adefa02d97acc
Daniel Lovegrove <d.lovegrove11@gmail.com>
2017-11-21 12:32:51 -0600

    Add Client files (See ECE3740-P3.3)

---
+++ AndroidGUIClient/app/src/main/java/cmd/UserCommandInvoker.java
@@ -0,0 +1,61 @@
+package cmd;
+
+import java.lang.reflect.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+/**
+ *
+ * @author loveboat
+ */
+public class UserCommandInvoker implements commandinterface.Command {
+    private commandinterface.Command userCommand;
+    // private final cmd.UserCommandReceiver commandReceiver;
+    private final userinterface.Userinterface UI;
+    private final client.Client myClient;
+    private final ExecutorService executor;
+
+    public UserCommandInvoker(userinterface.Userinterface ui, client.Client client) {
+        this.UI = ui;
+        // Allow four threads to be ran concurrently
+        this.executor = Executors.newFixedThreadPool(4);
+        myClient = client;
+        // this.commandReceiver = new cmd.UserCommandReceiver(ui, client);
+    }
+
+    @Override
+    public void execute(final String command) {
+        if (!command.equals("")) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    String secureCommand = "cmd." + command + "Command";
+                    try {
+                        // Create a parameter array for the constructor of the object
+                        Class[] parameters = new Class[1];
+                        parameters[0] = cmd.UserCommandReceiver.class;
+
+                        // Get the Class corresponding to the command
+                        Class newObject = Class.forName(secureCommand);
+
+                        // Get the class's constructor pattern
+                        Constructor<commandinterface.Command> objCtor =
+                                newObject.getDeclaredConstructor(parameters);
+
+                        // Create a new instance of the Class
+                        userCommand = objCtor.newInstance(new UserCommandReceiver(UI, myClient));
+
+                        // Execute the command
+                        userCommand.execute(secureCommand);
+
+                    } catch (ClassNotFoundException | NoSuchMethodException e) {
+                        UI.update("Invalid command.");
+                    } catch (InstantiationException | IllegalAccessException |
+                            InvocationTargetException ex) {
+                        UI.update("Error instantiating command class: " + ex.toString());
+                    }
+                }
+            });
+        }
+    }
+}
